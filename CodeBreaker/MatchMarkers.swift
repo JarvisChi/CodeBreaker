e3e53df8593e87382a3aa67f4f6e1aeb4b3268d0
JarvisChi <o0taiwan0o@gmail.com>
2026-01-04 14:47:23 -0500

    Assignment 1 exact match

---
+++ CodeBreaker/MatchMarkers.swift
@@ -0,0 +1,74 @@
+//
+//  MatchMarkers.swift
+//  CodeBreaker
+//
+//  Created by Dan Chi on 1/4/26.
+//
+
+import SwiftUI
+
+struct MatchMarkers: View {
+    let matches: [Match]
+    var body: some View {
+        HStack{
+            VStack{
+                matchMarker(peg: 0)
+                matchMarker(peg: 1)
+            }
+            VStack{
+                matchMarker(peg: 2)
+                matchMarker(peg: 3)
+            }
+            VStack{
+                matchMarker(peg: 4)
+                matchMarker(peg: 5)
+            }
+        }
+        .aspectRatio(1, contentMode: .fit)
+    }
+
+    func matchMarker(peg: Int) -> some View {
+        let exactCount = matches.count(where: {match in match == .exact })
+        let foundCount = matches.count(where: {match in match != .nomatch})
+        return Circle()
+            .fill(peg < exactCount ? Color.primary : Color.clear)
+            .strokeBorder(peg < foundCount ? Color.primary : Color.clear, lineWidth: 2)
+    }
+}
+
+enum Match {
+    case nomatch
+    case exact
+    case inexact
+}
+
+struct MatchMarkers_Previews: View{
+    let count: Int
+    let matches: [Match]
+    var body: some View{
+        HStack{
+            pegs(count: count)
+            MatchMarkers(matches: matches)
+        }
+        .padding()
+    }
+    func pegs(count: Int) -> some View {
+        ForEach(0..<count, id: \.self) { _ in
+            Circle()
+                .frame(width: 40, height: 40)
+        }
+    }
+}
+
+#Preview {
+    VStack(alignment: .leading){
+        MatchMarkers_Previews(count: 3, matches: [.nomatch,.exact,.inexact,.inexact])
+        MatchMarkers_Previews(count: 3, matches: [.exact])
+        MatchMarkers_Previews(count: 4, matches: [.exact, .exact, .inexact, .inexact])
+        MatchMarkers_Previews(count: 4, matches: [.nomatch,.exact,.inexact])
+        MatchMarkers_Previews(count: 6, matches: [.nomatch,.exact,.exact, .exact,.inexact])
+        MatchMarkers_Previews(count: 6, matches: [.nomatch,.exact,.exact, .exact,.inexact,.inexact,.inexact])
+        MatchMarkers_Previews(count: 5, matches: [.nomatch,.exact,.exact,.inexact,.inexact,.inexact])
+        MatchMarkers_Previews(count: 5, matches: [.nomatch,.exact,.inexact,.inexact])
+    }
+}
