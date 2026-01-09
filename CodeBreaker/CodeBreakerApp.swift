714878edb66fc38b1d49540ff55fa706a5599fca
JarvisChi <o0taiwan0o@gmail.com>
2026-01-08 19:19:49 -0500

    Lecture 4

--- CodeBreaker/CodeBreakerApp.swift
+++ CodeBreaker/CodeBreakerApp.swift
@@ -25,7 +25,7 @@ struct CodeBreakerApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CodeBreakerView()
         }
         .modelContainer(sharedModelContainer)
     }
